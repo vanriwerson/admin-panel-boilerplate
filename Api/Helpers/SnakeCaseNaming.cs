925dee8bb4c99678b1b7d215e111dbbb9ae48fa0
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-10 15:46:49 -0300

    feat: helper para tratamento de convenções PascalCase snake_case

---
+++ Api/Helpers/SnakeCaseNaming.cs
@@ -0,0 +1,31 @@
+namespace Api.Helpers
+{
+  public static class SnakeCaseNamingHelper
+  {
+    /// <summary>
+    /// Converte PascalCase para snake_case
+    /// </summary>
+    public static string ToSnakeCase(string input)
+    {
+      if (string.IsNullOrWhiteSpace(input))
+        return input;
+
+      var buffer = new System.Text.StringBuilder();
+      for (int i = 0; i < input.Length; i++)
+      {
+        var c = input[i];
+        if (char.IsUpper(c))
+        {
+          if (i > 0)
+            buffer.Append('_');
+          buffer.Append(char.ToLower(c));
+        }
+        else
+        {
+          buffer.Append(c);
+        }
+      }
+      return buffer.ToString();
+    }
+  }
+}
