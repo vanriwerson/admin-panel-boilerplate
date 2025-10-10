03d246c27c83d922d8621dde1a1ff90c16680f51
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-10 15:59:11 -0300

    feat: dtos da entidade User

---
+++ Api/Dtos/UserDtos/UserReadDto.cs
@@ -0,0 +1,12 @@
+namespace Api.Dtos
+{
+  public class UserReadDto
+  {
+    public int Id { get; set; }
+    public string Username { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string FullName { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+  }
+}
---
+++ Api/Dtos/UserDtos/UserUpdateDto.cs
@@ -0,0 +1,10 @@
+namespace Api.Dtos
+{
+  public class UserUpdateDto
+  {
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? FullName { get; set; }
+  }
+}
