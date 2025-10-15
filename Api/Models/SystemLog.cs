45029ba5d02e77355223b6ac6b4363480ecb38b7
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-15 14:51:09 -0300

    feat: cria novas entidades

---
+++ Api/Models/SystemLog.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models
+{
+  [Table("system_logs")]
+  public class SystemLog
+  {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("user_id")]
+    public required int UserId { get; set; }
+
+    [Required]
+    [Column("action")]
+    [MaxLength(255)]
+    public required string Action { get; set; }
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [ForeignKey(nameof(UserId))]
+    public User? User { get; set; }
+  }
+}
