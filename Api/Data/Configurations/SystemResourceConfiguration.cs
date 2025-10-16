39d37d043b5fd29d984e352f437f0bf7376ed7f4
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-16 09:08:38 -0300

    refactor: adequa relacionamentos ao soft delete

---
+++ Api/Data/Configurations/SystemResourceConfiguration.cs
@@ -0,0 +1,23 @@
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.Configurations
+{
+  public class SystemResourceConfiguration : IEntityTypeConfiguration<SystemResource>
+  {
+    public void Configure(EntityTypeBuilder<SystemResource> builder)
+    {
+      builder.HasIndex(r => r.Name).IsUnique();
+
+      builder.Property(r => r.Name)
+             .IsRequired()
+             .HasMaxLength(100);
+
+      builder.HasMany<AccessPermission>()
+             .WithOne(ap => ap.SystemResource)
+             .HasForeignKey(ap => ap.SystemResourceId)
+             .OnDelete(DeleteBehavior.Restrict);
+    }
+  }
+}
