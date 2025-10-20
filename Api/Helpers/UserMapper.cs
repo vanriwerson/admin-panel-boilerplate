569b3b92a9fe45061fe78761b9caa2f83faa5403
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-20 13:19:30 -0300

    feat: mapper para UserReadDto

---
+++ Api/Helpers/UserMapper.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using Api.Dtos;
+using Api.Models;
+
+namespace Api.Helpers
+{
+  public static class UserMapper
+  {
+    public static UserReadDto MapToUserReadDto(User user)
+    {
+      if (user == null)
+        return null;
+
+      return new UserReadDto
+      {
+        Id = user.Id,
+        Username = user.Username,
+        Email = user.Email,
+        FullName = user.FullName,
+        CreatedAt = user.CreatedAt,
+        UpdatedAt = user.UpdatedAt,
+        Permissions = user.AccessPermissions?
+              .Select(ap => new AccessPermissionReadDto
+              {
+                Id = ap.Id,
+                UserId = ap.UserId,
+                SystemResourceId = ap.SystemResourceId,
+                SystemResource = ap.SystemResource != null
+                      ? new SystemResourceOptionDto
+                      {
+                        Id = ap.SystemResource.Id,
+                        Name = ap.SystemResource.Name,
+                        ExhibitionName = ap.SystemResource.ExhibitionName
+                      }
+                      : null
+              })
+              .ToList() ?? new List<AccessPermissionReadDto>()
+      };
+    }
+  }
+}
