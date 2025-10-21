94d162159ddf1c8d84a67b06da9387aaa69845db
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-21 16:01:25 -0300

    feat: helper para padronização de mensagens de SystemLog.Action

---
+++ Api/Helpers/LogActionDescribe.cs
@@ -0,0 +1,23 @@
+namespace Api.Helpers
+{
+  public static class LogActionDescribe
+  {
+    public static string Create(string entity, int id)
+        => $"create {entity} id: {id}";
+
+    public static string Update(string entity, int id)
+        => $"update {entity} id: {id}";
+
+    public static string Delete(string entity, int id)
+        => $"delete {entity} id: {id}";
+
+    public static string Login(string username)
+        => $"user {username} logou no sistema";
+
+    public static string NewPasswordRequest(string username)
+        => $"user {username} solicitou reset de senha";
+
+    public static string PasswordReset(string username)
+        => $"user {username} alterou a senha";
+  }
+}
