a91968f800ee58eefdb31ca0b264e863f867c2d4
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-22 09:59:35 -0300

    fix: extrai id correto do user logado pelo token

--- Api/Services/SystemLogsServices/CreateSystemLog.cs
+++ Api/Services/SystemLogsServices/CreateSystemLog.cs
@@ -7,23 +7,28 @@ namespace Api.Services.SystemLogsServices
   public class CreateSystemLog
   {
     private readonly IGenericRepository<SystemLog> _repository;
+    private readonly CurrentAuthUser _currentAuthUser;
 
-    public CreateSystemLog(IGenericRepository<SystemLog> repository)
+    public CreateSystemLog(IGenericRepository<SystemLog> repository, CurrentAuthUser currentAuthUser)
     {
       _repository = repository;
+      _currentAuthUser = currentAuthUser;
     }
 
-    public async Task ExecuteAsync(int userId, string action)
+    public async Task ExecuteAsync(string action, int? userId = null)
     {
+      var logUserId = userId ?? _currentAuthUser.GetId();
+
       var log = new SystemLog
       {
-        UserId = userId,
+        UserId = logUserId,
         Action = action,
         CreatedAt = DateTime.UtcNow
       };
 
       await _repository.CreateAsync(log);
-      Console.WriteLine($"[AUDIT LOG] {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} | user_id: {userId} | action: {action}");
+
+      Console.WriteLine($"[AUDIT LOG] {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} | user_id: {logUserId} | action: {action}");
     }
   }
 }
