6d34fa23af40c2a4bd02a44dfbba84fe053b454a
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-23 16:45:38 -0300

    refactor: hook vira atalho para o context

--- WebApp/src/hooks/useAuth.ts
+++ WebApp/src/hooks/useAuth.ts
@@ -1,45 +1,8 @@
-import { useState } from 'react';
-import api from '../api';
-import type {
-  AuthUser,
-  ExternalLoginPayload,
-  LoginPayload,
-  LoginResponse,
-} from '../interfaces';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts';
 
 export function useAuth() {
-  const [token, setToken] = useState<string | null>(
-    localStorage.getItem('token')
-  );
-  const [authUser, setAuthUser] = useState<AuthUser | null>(null);
-
-  const handleAuthData = (data: LoginResponse) => {
-    setAuthUser({
-      username: data.username,
-      fullName: data.fullName,
-      permissions: data.permissions,
-    });
-    setToken(data.token);
-  };
-
-  async function login(payload: LoginPayload) {
-    const { data } = await api.post('/auth/login', payload);
-    localStorage.setItem('token', data.token);
-
-    handleAuthData(data);
-  }
-
-  async function externalLogin(payload: ExternalLoginPayload) {
-    const { data } = await api.post('/auth/external', payload);
-    localStorage.setItem('token', data.token);
-
-    handleAuthData(data);
-  }
-
-  function logout() {
-    localStorage.removeItem('token');
-    setToken(null);
-  }
-
-  return { token, authUser, login, externalLogin, logout };
+  const context = useContext(AuthContext);
+  if (!context) throw new Error('useAuth must be used within an AuthProvider');
+  return context;
 }
