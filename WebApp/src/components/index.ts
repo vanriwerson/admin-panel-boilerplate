aa2ae2c1d58fb80f0cd8e0c600732976c3c357b3
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-23 16:45:13 -0300

    feat: modal de requisição de reset de senha

--- WebApp/src/components/index.ts
+++ WebApp/src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as LoginForm } from './LoginForm';
+export { default as PasswordResetRequestModal } from './PasswordResetRequestModal';
+export { default as SidePanel } from './SidePanel';
 export { default as UserForm } from './UserForm';
 export { default as UserTable } from './UserTable';
