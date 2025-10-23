904d1a2a7aa6c8d0ec0b6107de5772db3269796a
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-23 16:46:42 -0300

    feat: estrutura básica das paginas para teste de navegação

--- WebApp/src/pages/index.ts
+++ WebApp/src/pages/index.ts
@@ -1,2 +1,6 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Login } from './Login';
+export { default as PasswordReset } from './PasswordReset';
+export { default as Reports } from './Reports';
+export { default as Resources } from './Resources';
+export { default as Users } from './Users';
