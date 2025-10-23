ecde730890324b7d16b4b032e3cac788bf4e26e5
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-23 16:19:55 -0300

    feat: cria contexto de autenticação

---
+++ WebApp/src/contexts/AuthContext.tsx
@@ -0,0 +1,95 @@
+import { createContext, useState, useEffect, type ReactNode } from 'react';
+import type {
+  AuthUser,
+  ExternalLoginPayload,
+  LoginPayload,
+  LoginResponse,
+  PasswordResetPayload,
+} from '../interfaces';
+import {
+  externalLogin,
+  login,
+  requestPasswordReset,
+  resetPassword,
+} from '../services';
+
+interface AuthContextType {
+  token: string | null;
+  authUser: AuthUser | null;
+  handleLogin: (payload: LoginPayload) => Promise<void>;
+  handleExternalLogin: (payload: ExternalLoginPayload) => Promise<void>;
+  handlePasswordResetRequest: (email: string) => Promise<string>;
+  handlePasswordReset: (payload: PasswordResetPayload) => Promise<string>;
+  handleLogout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem('token')
+  );
+  const [authUser, setAuthUser] = useState<AuthUser | null>(null);
+
+  const handleAuthData = (data: LoginResponse) => {
+    const userData: AuthUser = {
+      username: data.username,
+      fullName: data.fullName,
+      permissions: data.permissions,
+    };
+    setAuthUser(userData);
+    setToken(data.token);
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('authUser', JSON.stringify(userData));
+  };
+
+  async function handleLogin(payload: LoginPayload) {
+    const data = await login(payload);
+    handleAuthData(data);
+  }
+
+  async function handleExternalLogin(payload: ExternalLoginPayload) {
+    const data = await externalLogin(payload);
+    handleAuthData(data);
+  }
+
+  async function handlePasswordResetRequest(email: string): Promise<string> {
+    return await requestPasswordReset(email);
+  }
+
+  async function handlePasswordReset(
+    payload: PasswordResetPayload
+  ): Promise<string> {
+    return await resetPassword(payload);
+  }
+
+  function handleLogout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('authUser');
+    setToken(null);
+    setAuthUser(null);
+  }
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem('authUser');
+    if (savedUser) setAuthUser(JSON.parse(savedUser));
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        authUser,
+        handleLogin,
+        handleExternalLogin,
+        handlePasswordResetRequest,
+        handlePasswordReset,
+        handleLogout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
---
+++ WebApp/src/contexts/index.ts
@@ -0,0 +1,2 @@
+export { default as AuthContext } from './AuthContext';
+export { AuthProvider } from './AuthContext';
