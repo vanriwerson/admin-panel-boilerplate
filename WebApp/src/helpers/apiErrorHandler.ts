4b27c03d2d8505c93c850f620e8f453b29da7774
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-23 16:21:20 -0300

    feat: helper para tipagem de erro da api

---
+++ WebApp/src/helpers/apiErrorHandler.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export function getErrorMessage(error: unknown): string {
+  if (axios.isAxiosError(error)) {
+    return (
+      error.response?.data?.error ||
+      error.message ||
+      'Erro desconhecido no servidor.'
+    );
+  }
+
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  return 'Erro inesperado.';
+}
--- WebApp/src/helpers/index.ts
+++ WebApp/src/helpers/index.ts
@@ -1,1 +1,2 @@
 export { menuItems } from './menuItems';
+export { getErrorMessage } from './apiErrorHandler';
