382c3899509166cc4fa68c6b7387eda54fc3cc71
Bruno Riwerson Silva <riwerson@gmail.com>
2025-11-03 16:53:35 -0300

    feat: cria serviço de envio de e-mail

---
+++ Api/Services/AuthServices/EmailService.cs
@@ -0,0 +1,30 @@
+using Resend;
+using System.Threading.Tasks;
+
+namespace Api.Services
+{
+  public class EmailService
+  {
+    private readonly ResendClient _resendClient;
+
+    public EmailService(ResendClient resendClient)
+    {
+      _resendClient = resendClient;
+    }
+
+    public async Task SendPasswordResetEmailAsync(string toEmail, string resetLink)
+    {
+      await _resendClient.EmailSendAsync(new EmailMessage
+      {
+        From = "Suporte <no-reply@admin-panel.com>",
+        To = toEmail,
+        Subject = "Redefinição de senha",
+        HtmlBody = $@"
+                    <h3>Você solicitou uma redefinição de senha</h3>
+                    <p>Clique no link abaixo para criar uma nova senha:</p>
+                    <p><a href='{resetLink}' target='_blank'>Redefinir minha senha</a></p>
+                    <p>Esse link é válido por 15 minutos.</p>"
+      });
+    }
+  }
+}
