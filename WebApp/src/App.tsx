b2bff24594d01adc35a1c1763df63b821ad36071
Bruno Riwerson Silva <riwerson@gmail.com>
2025-11-05 12:01:05 -0300

    feat: aplica toggle do dark mode

--- WebApp/src/App.tsx
+++ WebApp/src/App.tsx
@@ -1,16 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import router from './routes';
-import theme from './theme';
-import { AuthProvider } from './contexts';
+import { AuthProvider, ThemeModeProvider } from './contexts';
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeModeProvider>
       <CssBaseline />
       <AuthProvider>
         <RouterProvider router={router} />
       </AuthProvider>
-    </ThemeProvider>
+    </ThemeModeProvider>
   );
 }
--- WebApp/src/layouts/DefaultLayout/index.tsx
+++ WebApp/src/layouts/DefaultLayout/index.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Box, IconButton, Toolbar } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { SidePanel } from '../../components';
+import { useThemeMode } from '../../hooks';
 
 export default function DefaultLayout() {
   const [open, setOpen] = useState(true);
+  const { mode, toggleTheme } = useThemeMode();
   const navigate = useNavigate();
 
   const handleNavigate = (route: string) => {
@@ -18,11 +20,19 @@ export default function DefaultLayout() {
       <SidePanel open={open} onNavigate={handleNavigate} />
 
       <Box component="main" sx={{ flexGrow: 1 }}>
-        <Toolbar>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
           <IconButton onClick={() => setOpen(!open)}>
             <FontAwesomeIcon icon={faBars} />
           </IconButton>
+
+          <IconButton onClick={toggleTheme}>
+            <FontAwesomeIcon
+              icon={mode === 'light' ? faMoon : faSun}
+              size="sm"
+            />
+          </IconButton>
         </Toolbar>
+
         <Outlet />
       </Box>
     </Box>
