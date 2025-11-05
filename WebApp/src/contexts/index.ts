d3f9d2cb45476b0ac4260a2b8f2b1e3ab86ea346
Bruno Riwerson Silva <riwerson@gmail.com>
2025-11-05 12:00:40 -0300

    feat: hook e contexto para o tema da aplicação

--- WebApp/src/contexts/index.ts
+++ WebApp/src/contexts/index.ts
@@ -1,2 +1,4 @@
 export { default as AuthContext } from './AuthContext';
+export { default as ThemeContext } from './ThemeContext';
 export { AuthProvider } from './AuthContext';
+export { ThemeModeProvider } from './ThemeContext';
--- WebApp/src/hooks/index.ts
+++ WebApp/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useAuth } from './useAuth';
 export { useReports } from './useReports';
 export { useSystemResources } from './useSystemResources';
+export { useThemeMode } from './useThemeMode';
 export { useUsers } from './useUsers';
