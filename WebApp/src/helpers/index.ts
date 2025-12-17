cf49211af4b0bcfad08584ca7ac8b5e5675f39ee
Bruno Riwerson Silva <riwerson@gmail.com>
2025-12-17 16:30:34 -0300

    feat: helper para identificação de entidade pelas chaves

--- WebApp/src/helpers/index.ts
+++ WebApp/src/helpers/index.ts
@@ -1,5 +1,6 @@
 export { buildStatsCards } from './buildStatsCards';
 export { cleanStates } from './cleanStates';
+export { detectEntityType } from './detectEntityType';
 export { getErrorMessage } from './apiErrorHandler';
 export { mapSystemResourcesToFormValue } from './mapSystemResourcesToFormValue';
 export { menuItems } from './menuItems';
