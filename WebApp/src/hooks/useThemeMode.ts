fa055d8910548bcbd7a49d82494a5be95e2eebc0
Bruno Riwerson Silva <riwerson@gmail.com>
2025-12-18 08:09:56 -0300

    chore: ajusta mensagem de erro

--- WebApp/src/hooks/useThemeMode.ts
+++ WebApp/src/hooks/useThemeMode.ts
@@ -4,7 +4,9 @@ import { ThemeContext } from '../contexts';
 export const useThemeMode = () => {
   const context = useContext(ThemeContext);
   if (!context) {
-    throw new Error('useThemeMode deve ser usado dentro de ThemeModeProvider');
+    throw new Error(
+      'useThemeMode deve ser usado dentro de <ThemeModeProvider>'
+    );
   }
   return context;
 };
