bdfae27bb4fd9d818c08609ca4b8265ef759da57
DTI-SISTEMAS-ARARAS-SP <dtisistemas@araras.sp.gov.br>
2026-02-04 14:59:52 -0300

    refactor: suprime props não utilizadas

--- Api/Helpers/UserMapper.cs
+++ Api/Helpers/UserMapper.cs
@@ -15,16 +15,11 @@ namespace Api.Helpers
                 Username = user.Username,
                 Email = user.Email,
                 FullName = user.FullName,
-                Active = user.Active,
-                CreatedAt = user.CreatedAt,
-                UpdatedAt = user.UpdatedAt,
-
                 Permissions = user.AccessPermissions?
                   .Where(ap => ap.SystemResource != null && ap.SystemResource.Active)
                   .Select(ap => new SystemResourceSelectDto
                   {
                       Id = ap.SystemResource!.Id,
-                      Name = ap.SystemResource.Name,
                       ExhibitionName = ap.SystemResource.ExhibitionName
                   })
                   .ToList() ?? new List<SystemResourceSelectDto>()
